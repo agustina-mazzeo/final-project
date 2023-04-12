3578ccc90ff33ababf42c3cfe0ac102cbd7054c4
agustina-mazzeo <agustina.mazzeo@decemberlabs.com>
2023-04-12 14:18:58 -0300

    Project structure

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import validateEnv from './utils/validateEnv';
+import { App } from './app';
+
+// App Variables
+dotenv.config();
+validateEnv();
+(async () => {
+  try {
+    const app = new App();
+    app.start();
+  } catch (err) {
+    console.log(`App did not start, error: ${err}`);
+  }
+})();
