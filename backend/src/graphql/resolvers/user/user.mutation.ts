61abab040405835df80c13823b7d611ed148aebb
agustina-mazzeo <agustina.mazzeo@decemberlabs.com>
2023-05-19 13:56:19 -0300

    refactor errors

--- backend/src/graphql/resolvers/user/user.mutation.ts
+++ backend/src/graphql/resolvers/user/user.mutation.ts
@@ -1,16 +1,33 @@
-import { UnauthorizedError } from '../../../interfaces';
+import { CustomError, UnauthorizedError } from '../../../interfaces';
 import { createToken } from '../../../api/utils/helpers';
 import { Context } from '../../context';
 import { compare } from 'bcrypt';
+import { GraphQLError } from 'graphql';
 export const userMutations = {
   createUser: async (parent: undefined, args: Record<string, any>, contextValue: Context) => {
-    const { name, email } = await contextValue.dataSources.userWriteService.create(args.user);
-    return { name, email };
+    try {
+      const { name, email } = await contextValue.dataSources.userWriteService.create(args.user);
+      return { name, email };
+    } catch (error: any) {
+      if (error instanceof CustomError)
+        throw new GraphQLError(error.message, {
+          extensions: { code: error.errorType },
+        });
+      throw error;
+    }
   },
   login: async (parent: undefined, { user }: any, contextValue: Context) => {
-    const savedUser = await contextValue.dataSources.userReadService.getByEmail(user.email);
-    if (!(await compare(user.password, savedUser.password))) {
-      throw new UnauthorizedError('Invalid credentials');
-    } else return { user: { name: savedUser, email: savedUser.email }, token: createToken(savedUser) };
+    try {
+      const savedUser = await contextValue.dataSources.userReadService.getByEmail(user.email);
+      if (!(await compare(user.password, savedUser.password))) {
+        throw new UnauthorizedError('Invalid credentials');
+      } else return { user: { name: savedUser, email: savedUser.email }, token: createToken(savedUser) };
+    } catch (error: any) {
+      if (error instanceof CustomError)
+        throw new GraphQLError(error.message, {
+          extensions: { code: error.errorType },
+        });
+      throw error;
+    }
   },
 };
